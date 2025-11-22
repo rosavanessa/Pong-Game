6cc71b7597af47e35db9209e2086ef926b88e78c
Vanessa Rosa <vanessanavalayo@gmail.com>
2025-11-22 15:02:16 +0300

    x and y speed of ball

--- game.py
+++ game.py
@@ -24,8 +24,9 @@ def main():
 
 paddle_1_rect = pygame.Rect(30, 0, 7, 100)
 paddle_2_rect = pygame.Rect(SCREEN_WIDTH - 50, 0, 7, 100)
-
 paddle_1_move = 0
 paddle_2_move = 0
-
+ball_rect = pygame.Rect(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 25, 25)
+ball_accel_x = random.randint(2, 4) * 0.1
+ball_accel_y = random.randint(2, 4) * 0.1
 
