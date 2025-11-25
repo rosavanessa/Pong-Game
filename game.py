9c8e615ce406a30c67b816b9ffa9c0adc2c967f1
Vanessa Rosa <vanessanavalayo@gmail.com>
2025-11-25 17:18:53 +0300

    draw rectangle

--- game.py
+++ game.py
@@ -11,26 +11,38 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Pong")
 
-    while True:
-        """
-        Set the back ground color to black
-        needs to be called everytime the game updates
-        """
-        screen.fill(COLOR_BLACK)
+    paddle_1_rect = pygame.Rect(30, 0, 7, 100)
+    paddle_2_rect = pygame.Rect(SCREEN_WIDTH - 50, 0, 7, 100)
+    paddle_1_move = 0
+    paddle_2_move = 0
 
+    ball_rect = pygame.Rect(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 25, 25)
+    ball_accel_x = random.randint(2, 4) * 0.1
+    ball_accel_y = random.randint(2, 4) * 0.1
+
+    if random.randint(1, 2) == 1:
+        ball_accel_x *= -1
+    if random.randint(1, 2) == 1:
+        ball_accel_y *= -1
+
+    while True:
+        # EVENT LOOP
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-paddle_1_rect = pygame.Rect(30, 0, 7, 100)
-paddle_2_rect = pygame.Rect(SCREEN_WIDTH - 50, 0, 7, 100)
-paddle_1_move = 0
-paddle_2_move = 0
-ball_rect = pygame.Rect(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 25, 25)
-ball_accel_x = random.randint(2, 4) * 0.1
-ball_accel_y = random.randint(2, 4) * 0.1
+        # CLEAR SCREEN
+        screen.fill(COLOR_BLACK)
 
-if random.randint(1, 2) == 1:
-    ball_accel_x *= -1
-if random.randint(1, 2) == 1:
-    ball_accel_y *= -1
+        # DRAW PADDLES
+        pygame.draw.rect(screen, COLOR_WHITE, paddle_1_rect)
+        pygame.draw.rect(screen, COLOR_WHITE, paddle_2_rect)
+
+        # DRAW BALL
+        pygame.draw.rect(screen, COLOR_WHITE, ball_rect)
+
+        # UPDATE DISPLAY
+        pygame.display.update()
+
+if __name__ == "__main__":
+    main()
